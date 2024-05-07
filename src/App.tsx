da549ebaea9c4d9db732c66f2df7f6ea01b17ef0
Tomas <tomasoldenburg@hotmail.com>
2024-05-07 15:35:49 -0300

    style: add react-icons(Feather) and use it for each card component passing it as prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { CardComponent } from "./components/Card.tsx";
-
+import { FiDollarSign } from "react-icons/fi";
+import { FiClipboard } from "react-icons/fi";
+import { FiLink } from "react-icons/fi";
 function App() {
   return (
-    <main className="bg-gray-300/75 pt-8">
-      <h1 className="flex justify-start pl-4 antialiased uppercase text-5xl drop-shadow-md font-bold text-sky-950/90">
-        Expenses app
-      </h1>
-      <div className="p-8 flex flex-row justify-between">
-        <CardComponent title="Disponible"/>
-        <CardComponent title="Gastos totales" />
-        <CardComponent title="Servicios" />
+    <main className="flex-1 bg-gray-300/75 pt-8">
+      <div className="container relative">
+        <h1 className="flex justify-start pl-4 antialiased uppercase text-5xl drop-shadow-md font-bold">
+          Expenses app
+        </h1>
+        <section>
+          <div className="relative space-y-4 p-8 pt-6">
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+              <CardComponent title="Disponible" IconCard={<FiDollarSign />} />
+              <CardComponent
+                title="Gastos totales"
+                IconCard={<FiClipboard />}
+              />
+              <CardComponent title="Servicios" IconCard={<FiLink />} />
+            </div>
+          </div>
+        </section>
       </div>
     </main>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,11 @@ import { useEffect, useState } from "react";
 import { Database } from "../types/schema";
 import { supabase } from "../../db/supabase";
 
-export function CardComponent({ title }: { title: string }) {
+interface Props {
+  title: string;
+  IconCard: JSX.Element;
+}
+export function CardComponent({ title, IconCard }: Props) {
   const [amount, setAmount] =
     useState<Database["public"]["Tables"]["expenses"]["Row"]["amount"]>();
 
@@ -23,21 +27,10 @@ export function CardComponent({ title }: { title: string }) {
     fetchAmount();
   }, []);
   return (
-    <Card className="w-[270px] ">
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+    <Card>
+      <CardHeader className="p-6 flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-sm">{title}</CardTitle>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          className="h-4 w-4 text-muted-foreground"
-        >
-          <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
-        </svg>
+        {IconCard}
       </CardHeader>
       <CardContent className="flex justify-between pt-2">
         <Label className="text-2xl font-bold ">
