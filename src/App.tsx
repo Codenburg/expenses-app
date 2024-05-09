7a889d5fd44b0b096011890da396e8f4353bf9ab
Tomas <tomasoldenburg@hotmail.com>
2024-05-08 21:10:07 -0300

    feat: add payment table

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { CardComponent } from "./components/Card.tsx";
 import { FiDollarSign } from "react-icons/fi";
 import { FiClipboard } from "react-icons/fi";
 import { FiLink } from "react-icons/fi";
+import PaymentTable from "./components/PaymentTable.tsx";
 function App() {
   return (
-    <main className="flex-1 bg-gray-300/75 pt-8">
+    <main className="flex-1 pt-8">
       <div className="container relative">
         <h1 className="flex justify-start pl-4 antialiased uppercase text-5xl drop-shadow-md font-bold">
           Expenses app
         </h1>
         <section>
-          <div className="relative space-y-4 p-8 pt-6">
+          <div className="relative space-y-4 pb-6 pt-6">
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
               <CardComponent title="Disponible" IconCard={<FiDollarSign />} />
               <CardComponent
@@ -19,6 +20,13 @@ function App() {
               />
               <CardComponent title="Servicios" IconCard={<FiLink />} />
             </div>
+          </div>
+        </section>
+        <section>
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7"> 
+          <div className="border rounded-xl shadow col-span-2">
+          <PaymentTable/>
+          </div>
           </div>
         </section>
       </div>
