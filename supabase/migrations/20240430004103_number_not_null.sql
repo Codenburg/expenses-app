e09a1dc5f3511d1cc07d1e504bf7589fef6b46ce
Tomas <tomasoldenburg@hotmail.com>
2024-06-10 17:52:20 -0300

    Feat: create a new migrations in the db -Generate new definitions types for db - Set query of amount available  query to  false ascending

--- supabase/migrations/20240430004103_number_not_null.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."expenses" alter column "amount" set default '0'::numeric;
-
-alter table "public"."expenses" alter column "amount" set not null;
-
-
--- supabase/seed.sql
+++
@@ -1,2 +0,0 @@
-insert into "expenses"(amount,category_id) values (5000, 1)
-insert into "categories"(category_name) values ("Supermercado")('Salud')('Deportes')('Hogar')('Electronica')('Cuentas')("Impuestos")('Indumentaria')('Transporte')
