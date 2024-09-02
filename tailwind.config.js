7550b33e5f8bd4cd2f94adc1d84f7347d50a2fcf
Tomas <tomasoldenburg@hotmail.com>
2024-09-01 21:52:59 -0300

    Dev: Move indexes to root project and add comment in tailwinds.config for the linter error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
+/* eslint-env node */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -34,4 +35,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
